6fafacb98cd2f23595ca2759d1ca179a1d3f8adc
Blaine Schmeisser <blainesch@gmail.com>
2016-06-01 11:46:50 -0700

    Add a copy image to clipboard example.

--- monitor.js
+++ monitor.js
@@ -1,67 +1,21 @@
-'use strict';
+'use strict'
 
-const Datastore = require('nedb')
+const clipboard = require('electron').clipboard
+const CappedCollection = require('./lib/cappedCollection')
 
-class CappedCollection {
-  constructor (name, size) {
-    this.MAX_SIZE = size
-    this.collection = new Datastore({
-      filename: './' + name + '.nedb',
-      autoload: true,
-    });
-  }
+const clipCollection = new CappedCollection('clipCollection', 5)
 
-  all () {
-    return new Promise((accept) => {
-      return this.collection
-        .find({})
-        .sort({ createdAt: 1 })
-        .exec((err, docs) => {
-          err ? reject(err) : accept(docs)
-        })
+// console.log('text', clipboard.readText())
+// console.log('image empty', clipboard.readImage().isEmpty())
+
+clipCollection.last().then((doc) => {
+  const currentClip = clipboard.readText()
+  if (!doc || doc.clip !== currentClip) {
+    return clipCollection.insert({
+      type: 'text/html',
+      clip: currentClip,
     })
   }
-
-  remove (ids) {
-    return new Promise((accept) => {
-      this.collection.remove({ _id: { $in: ids } }, { multi: true }, (err, num) => {
-        err ? reject(err) : accept(num)
-      })
-    })
-  }
-
-  reduce () {
-    return this.all().then((docs) => {
-      const excessDocs = docs.slice(0, Math.max(0, docs.length - this.MAX_SIZE))
-      const excessIds = excessDocs.map((doc) => doc._id)
-      return this.remove(excessIds)
-    })
-  }
-
-  insert (doc) {
-    doc.createdAt = doc.createdAt || new Date()
-    return new Promise((accept) => {
-      this.collection.insert(doc, (err) => {
-        err ? reject(err) : accept()
-      })
-    }).then(() => {
-      return this.reduce()
-    })
-  }
-}
-
-/*
-  const clipboard = require('electron').clipboard
-  setInterval(() => {
-    console.log('text', clipboard.readText())
-    console.log('html', clipboard.readHTML())
-    console.log('rtf', clipboard.readRTF())
-  }, 500)
-*/
-var clipboard = new CappedCollection('clipboard', 5)
-clipboard.all().then((docs) => {
-  console.log({docs})
-  clipboard.insert({
-    type: 'meow',
-  })
+}).then(() => {
+  process.exit(0)
 })
